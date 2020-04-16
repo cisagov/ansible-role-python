a7527216617588c243b9b1503f53ed01faf864b3
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-16 16:39:11 -0400

    Only install python2 for Debian 9
    We treat Debian 9 as a special case, since the CyHy AMIs (and only the
    CyHy AMIs) are built on it.  Therefore it requires support for
    python2.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,30 +12,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["python", "python3"])
-def test_python_amazon(host, pkg):
-    """Test that the appropriate packages were installed.
-
-    Amazon Linux is behind Fedora, so that is why we need a separate
-    test for that distribution.  If we tested an older version of
-    Fedora it would use this test as well.
-    """
-    if host.system_info.distribution == "amzn":
-        assert host.package(pkg).is_installed
-
-
-@pytest.mark.parametrize("pkg", ["python", "python3"])
+@pytest.mark.parametrize("pkg", ["python3"])
 def test_python_debian(host, pkg):
     """Test that the appropriate packages were installed."""
     if (
-        host.system_info.distribution == "debian"
+        (
+            host.system_info.distribution == "debian"
+            and host.system_info.release != "9.12"
+        )
+        or host.system_info.distribution == "redhat"
         or host.system_info.distribution == "kali"
+        or host.system_info.distribution == "amzn"
     ):
         assert host.package(pkg).is_installed
 
 
-@pytest.mark.parametrize("pkg", ["python2", "python3"])
+@pytest.mark.parametrize("pkg", ["python", "python3"])
 def test_python_fedora(host, pkg):
     """Test that the appropriate packages were installed."""
-    if host.system_info.distribution == "fedora":
+    if host.system_info.distribution == "debian" and host.system_info.release == "9.12":
         assert host.package(pkg).is_installed
