aeeea22a78114ebea311ee63a2993fdef14889af
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-05-18 15:29:47 -0400

    Add a second Molecule scenario
    This covers the case where Python 2 is installed alongside Python 3.

---
+++ molecule/python2/tests/test_python2.py
@@ -0,0 +1,49 @@
+"""Module containing the tests for the python2 scenario."""
+
+# Standard Python Libraries
+import os
+
+# Third-Party Libraries
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+def test_python3_packages(host):
+    """Test that the appropriate Python 3 packages were installed."""
+    if host.system_info.distribution in ["amzn"]:
+        for p in ["python3", "python3-rpm"]:
+            assert host.package(p).is_installed
+    elif host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        for p in ["python3", "python3-apt", "python3-minimal"]:
+            assert host.package(p).is_installed
+    elif host.system_info.distribution in "fedora":
+        for p in ["python3", "python3-dnf"]:
+            assert host.package(p).is_installed
+    else:
+        assert (
+            False
+        ), f"Linux distribution {host.system_info.distribution} is not supported."
+
+
+def test_python2_packages(host):
+    """Test that the appropriate Python 2 packages were installed."""
+    if host.system_info.distribution in ["amzn"]:
+        for p in ["python"]:
+            assert host.package(p).is_installed
+    elif host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        if host.system_info.codename not in ["bionic", "stretch"]:
+            for p in ["python2", "python2-minimal"]:
+                assert host.package(p).is_installed
+        else:
+            for p in ["python", "python-minimal"]:
+                assert host.package(p).is_installed
+    elif host.system_info.distribution in ["fedora"]:
+        for p in ["python2.7"]:
+            assert host.package(p).is_installed
+    else:
+        assert (
+            False
+        ), f"Linux distribution {host.system_info.distribution} is not supported."
