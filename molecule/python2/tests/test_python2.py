7a80ed568ac813de0a3a117be10f1daadc50c81a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-05-19 15:14:24 -0400

    Install necessary packages (where possible) to allow Ansible to use Python 2
    Note that this can only be done for older Linux distributions such as
    Amazon Linux 2, Debian Stretch, and Debian Buster.  This is because
    Python 2 is well past EOL and therefore the necessary Python 2
    packages do not exist in newer distributions.

--- molecule/python2/tests/test_python2.py
+++ molecule/python2/tests/test_python2.py
@@ -14,14 +14,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_python2_packages(host):
     """Test that the appropriate Python 2 packages were installed."""
     if host.system_info.distribution in ["amzn"]:
-        for p in ["python"]:
+        for p in ["python", "python2-rpm"]:
             assert host.package(p).is_installed
     elif host.system_info.distribution in ["debian", "kali", "ubuntu"]:
-        if host.system_info.codename not in ["bionic", "stretch"]:
-            for p in ["python2", "python2-minimal"]:
+        if host.system_info.codename in ["bionic", "stretch"]:
+            for p in ["python", "python-apt", "python-minimal"]:
+                assert host.package(p).is_installed
+        elif host.system_info.codename in ["buster"]:
+            for p in ["python2", "python-apt", "python2-minimal"]:
                 assert host.package(p).is_installed
         else:
-            for p in ["python", "python-minimal"]:
+            for p in ["python2", "python2-minimal"]:
                 assert host.package(p).is_installed
     elif host.system_info.distribution in ["fedora"]:
         for p in ["python2.7"]:
