66b8562e2fd3a247f59488497dc76acb597f6350
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-03-28 10:08:09 -0400

    Rearrange test logic such that it will automatically work for future Debian releases
    Also drop logic for Bionic as we no longer support it.

--- molecule/python2/tests/test_python2.py
+++ molecule/python2/tests/test_python2.py
@@ -13,19 +13,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_python2_packages(host):
     """Test that the appropriate Python 2 packages were installed."""
-    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
-        if host.system_info.codename in ["bionic"]:
-            for p in ["python", "python-apt", "python-minimal"]:
-                assert host.package(p).is_installed
-        elif host.system_info.codename in ["buster"]:
+    if host.system_info.distribution in ["debian", "ubuntu"]:
+        if host.system_info.codename in ["buster"]:
             for p in ["python2", "python-apt", "python2-minimal"]:
                 assert host.package(p).is_installed
-        elif host.system_info.codename in ["bookworm", "trixie"]:
-            # Python 2 is not installed in this case
-            pass
-        else:
+        elif host.system_info.codename in ["bullseye"]:
             for p in ["python2", "python2-minimal"]:
                 assert host.package(p).is_installed
+        else:
+            # Python 2 is not installed in this case
+            pass
+    elif host.system_info.distribution in ["kali"]:
+        for p in ["python2", "python2-minimal"]:
+            assert host.package(p).is_installed
     elif host.system_info.distribution in ["fedora"]:
         for p in ["python2.7"]:
             assert host.package(p).is_installed
